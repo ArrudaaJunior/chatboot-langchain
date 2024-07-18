d637270efc23f62707dc15af7d629bde2928e4f0
ArrudaaJunior <juniorarruda15@gmail.com>
2024-07-18 15:35:13 -0300

    Utilizando LCEL para criar um roteiro de viagens

---
+++ langchain_lcel.py
@@ -0,0 +1,55 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SimpleSequentialChain
+from langchain.globals import set_debug
+from langchain_core.pydantic_v1 import Field, BaseModel
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+
+
+from openai import OpenAI
+
+set_debug(True)
+
+class Destino(BaseModel):
+    cidade = Field("ciadade a visitar")
+    motivo = Field("motivo pelo qual pe interessante visitar a ciadade")
+
+try:
+    from config import OPENAI_API_KEY
+except ImportError:
+    raise ImportError("Arquivo de configuração não encontrado. Verifique se o config.py está presente e configurado corretamente.")
+
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.5,
+    api_key=(OPENAI_API_KEY)
+)
+
+parseador = JsonOutputParser(pydantic_object=Destino)
+
+modelo_cidade = PromptTemplate(
+    template = """Sugira uma cidade dado meu interesse por {interesse}
+    {formatacao_de_saida}
+    """,
+    input_variables=["interesse"],
+    partial_variables={"formatacao_de_saida": parseador.get_format_instructions()}
+)
+
+modelo_restaurantes = ChatPromptTemplate.from_template(
+    "Sugira restaurantes populares entre locais em {cidade}"
+)
+
+modelo_cultural = ChatPromptTemplate.from_template(
+    "Sugira atividades e locais culturais em {cidade}"
+)
+
+parte1 = modelo_cidade | llm | parseador
+parte2 = modelo_restaurantes | llm | StrOutputParser()
+parte3 = modelo_cultural | llm | StrOutputParser()
+
+cadeia = (parte1 | parte2 | parte3)
+
+resultado = cadeia.invoke({"interesse": "praias"})
+print(resultado)
